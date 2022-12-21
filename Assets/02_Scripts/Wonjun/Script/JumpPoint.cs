93e3def662d4706f06f1b9a226707b5e4582f186
dlalsdb <nacfson@gmail.com>
2022-12-21 20:19:44 +0900

    ;

--- Assets/02_Scripts/Wonjun/Script/JumpPoint.cs
+++ Assets/02_Scripts/Wonjun/Script/JumpPoint.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class JumpPoint : MonoBehaviour
 { 
-    // Start is called before the first frame update
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
