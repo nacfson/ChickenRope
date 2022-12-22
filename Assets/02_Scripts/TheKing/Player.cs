2e1ee8f3c04565210ee71e75565e4852095f69a2
dlalsdb <nacfson@gmail.com>
2022-12-22 18:59:18 +0900

    dd

--- Assets/02_Scripts/TheKing/Player.cs
+++ Assets/02_Scripts/TheKing/Player.cs
@@ -63,7 +63,7 @@ public class Player : MonoBehaviour
     {
         if(other.gameObject.CompareTag("Ring")&& hook.isAttach)
         {
-            ChangeRopeHealth(-1);
+            //ChangeRopeHealth(-1);
         }
     }
 
