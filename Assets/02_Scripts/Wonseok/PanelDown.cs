d667156db05bd8a580626c1c23f96cb644a51a74
dlalsdb <nacfson@gmail.com>
2023-01-03 18:50:04 +0900

    dd

--- Assets/02_Scripts/Wonseok/PanelDown.cs
+++ Assets/02_Scripts/Wonseok/PanelDown.cs
@@ -5,14 +5,23 @@ using DG.Tweening;
 
 public class PanelDown : MonoBehaviour
 {
+    public Vector3 originPos;
     public void PanelMove()
     {
         transform.DOMoveY(550, 1f);
+        //transform.position = originPos;
+    }
+
+    public void InvokePanelMove()
+    {
+        Invoke("PanelMove", 2.5f);
     }
 
     private void Start()
     {
-        PanelMove(); 
+        //PanelMove(); 
+        originPos = transform.position;
+        GameManager.Instance.ClearAction += InvokePanelMove;
     }
 
     public void MenuButton()
